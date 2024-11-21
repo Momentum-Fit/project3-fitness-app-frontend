8cc66e68af4f4ea9c890d8343c675047b91fc0f6
fiona-eh <fionahulse@mac.com>
2024-11-21 16:56:42 +0100

    add homepage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Plans from "../components/Plans";
+
+function HomePage() {
+
+  return (
+    <>
+        <h1>Momentum Fit</h1>
+        <Plans />
+    </>
+  )
+}
+
+export default HomePage;
