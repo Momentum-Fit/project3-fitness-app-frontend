322ee74cff3af0e642e758be3a0b27a2d76fdcf6
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-21 09:04:41 +0100

    add empty HomePage component and route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./Pages/SignUpPage";
 import LoginPage from "./Pages/LoginPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <>
       <h1>this is the app</h1>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/auth/login" element={<LoginPage />} />
         <Route path="/auth/signup" element={<SignUpPage />} />
         <Route path="/*" element={<h1>Nothing to see</h1>} />
