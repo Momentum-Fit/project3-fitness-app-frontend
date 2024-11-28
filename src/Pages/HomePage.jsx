0d7320bf7a4340c6cca16579fe10efae55e6d273
fiona-eh <fionahulse@mac.com>
2024-11-28 17:13:09 +0100

    add styling and links for homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,6 +7,7 @@ function HomePage() {
     <>
       <section className="bg-#E5E8EB dark:bg-gray-900">
         <div className="container px-6 py-12 mx-auto">
+        <span className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white underline decoration-blue-500"><h1 className="mb-20 text-center">Momentum Fit</h1></span>
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
             <div>
               <h1 className="mt-4 text-xl font-semibold text-gray-800 dark:text-white">
@@ -71,16 +72,16 @@ function HomePage() {
         <div className="container px-6 py-10 mx-auto">
           <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
             explore our featured{" "}
-            <span className="underline decoration-blue-500">Workout plans</span>
+            <span className="underline decoration-blue-500">
+            <Link to="/plans"> Workout plans</Link></span>
           </h1>
 
           <p className="mt-4 text-gray-500 xl:mt-6 dark:text-gray-300">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
-            quam voluptatibus
+            Check out some of our most popular workout plans!
           </p>
 
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
-            <Link to={`http://localhost:5173/plans/${planId}`}>
+            <Link to="plans/673f02adee14f1ad848220ca">
               <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
                 <span className="inline-block text-blue-500 dark:text-blue-400">
                   <svg
@@ -135,7 +136,7 @@ function HomePage() {
                 </a>
               </div>
             </Link>
-            <Link to={`http://localhost:5173/plans/${planId}`}>
+            <Link to="plans/673f021fee14f1ad8482208a">
               <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
                 <span className="inline-block text-blue-500 dark:text-blue-400">
                   <svg
@@ -155,12 +156,12 @@ function HomePage() {
                 </span>
 
                 <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
-                  HIIT for Fat Loss
+                Balance & Burn
                 </h1>
 
                 <p className="text-gray-500 dark:text-gray-300">
                   High-intensity intervals to burn fat and build endurance
-                  quickly.
+                  mixed with strength exercises for muscle definition.
                 </p>
 
                 <a
@@ -184,7 +185,7 @@ function HomePage() {
                 </a>
               </div>
             </Link>
-            <Link to={`http://localhost:5173/plans/${planId}`}>
+            <Link to="/plans/673f0394ee14f1ad8482210a">
               <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
                 <span className="inline-block text-blue-500 dark:text-blue-400">
                   <svg
@@ -210,105 +211,6 @@ function HomePage() {
                 <p className="text-gray-500 dark:text-gray-300">
                   Progressive strength-building exercises to help you build
                   muscle and power.
-                </p>
-
-                <a
-                  href="#"
-                  className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </Link>
-            <Link to={`http://localhost:5173/plans`}>
-              <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
-                <span className="inline-block text-blue-500 dark:text-blue-400">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-8 h-8"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"
-                    />
-                  </svg>
-                </span>
-
-                <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
-                  Create Your Custom Plan
-                </h1>
-
-                <p className="text-gray-500 dark:text-gray-300">
-                  Get a personalized workout plan based on your fitness goals,
-                  schedule, and preferences.
-                </p>
-
-                <a
-                  href="#"
-                  className="inline-flex p-2 text-blue-500 capitalize transition-colors duration-300 transform bg-blue-100 rounded-full rtl:-scale-x-100 dark:bg-blue-500 dark:text-white hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                    />
-                  </svg>
-                </a>
-              </div>
-            </Link>
-            <Link to={`http://localhost:5173/profile`}>
-              <div className="p-8 space-y-3 border-2 border-blue-400 dark:border-blue-300 rounded-xl">
-                <span className="inline-block text-blue-500 dark:text-blue-400">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-8 h-8"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-                    />
-                  </svg>
-                </span>
-
-                <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
-                  Track Your Progress
-                </h1>
-
-                <p className="text-gray-500 dark:text-gray-300">
-                  Stay motivated by tracking your workouts, calories burned, and
-                  strength gains. See your progress over time and achieve your
-                  fitness goals faster.
                 </p>
 
                 <a
@@ -369,7 +271,7 @@ function HomePage() {
               <p className="text-gray-500 dark:text-gray-300">120</p>
 
               <a
-                href="#"
+                href="/plans"
                 className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500"
               >
                 <span className="mx-1">Get started now</span>
@@ -390,20 +292,26 @@ function HomePage() {
 
             <div className="flex flex-col items-center p-6 space-y-3 text-center bg-gray-100 rounded-xl dark:bg-gray-800">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full dark:text-white dark:bg-blue-500">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-                  />
-                </svg>
+              <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-8 h-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+                    />
+                  </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize dark:text-white">
@@ -413,10 +321,10 @@ function HomePage() {
               <p className="text-gray-500 dark:text-gray-300">5000</p>
 
               <a
-                href="#"
+                href="/about"
                 className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500"
               >
-                <span className="mx-1">Learn More About the Plans</span>
+                <span className="mx-1">Learn More About Momentum Fit</span>
                 <svg
                   className="w-4 h-4 mx-1 rtl:-scale-x-100"
                   fill="currentColor"
@@ -459,10 +367,10 @@ function HomePage() {
               </p>
 
               <a
-                href="#"
+                href="/auth/signup"
                 className="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500"
               >
-                <span className="mx-1">Customize a Workout Plan</span>
+                <span className="mx-1">Sign up Today & Customize a Workout Plan</span>
                 <svg
                   className="w-4 h-4 mx-1 rtl:-scale-x-100"
                   fill="currentColor"
